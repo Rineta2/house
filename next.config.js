caf4a1644800a4223d8bec651db0f6d84f3598b5
Rineta2 <rzkir571@gmail.com>
2024-04-27 09:31:48 +0700

    komit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { DeleteSourceMapsPlugin } = require('webpack-delete-sourcemaps-plugin');
+const withBundleAnalyzer = require('@next/bundle-analyzer')();
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')()
+const nextConfig = {
+  devtool: 'hidden-source-map',
+  plugins: [
+    new DeleteSourceMapsPlugin()
+  ]
+};
 
-module.exports =
-  process.env.ANALYZE === 'true' ? withBundleAnalyzer(nextConfig) : nextConfig
+module.exports = process.env.ANALYZE === 'true' ? withBundleAnalyzer(nextConfig) : nextConfig;
